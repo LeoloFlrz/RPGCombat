79fa5dbd8916308a485e3126298c1a14c68b4258
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-07-17 12:57:09 +0200

    created some tests

--- src/main/java/org/rpgcombat/Character.java
+++ src/main/java/org/rpgcombat/Character.java
@@ -1,4 +1,38 @@
 package org.rpgcombat;
 
 public class Character {
+    int health;
+    int level;
+    boolean alive;
+    int damage;
+
+    public Character(int health, int level, boolean alive, int damage) {
+        this.health = health;
+        this.level = level;
+        this.alive = alive;
+        this.damage = damage;
+    }
+
+    public int attack() {
+        return this.damage;
+    }
+    public int receiveDamage(int damage) {
+        return this.health -= damage;
+    }
+
+    public boolean isAlive() {
+        if (this.health <= this.damage) {
+            return alive = false;
+        } else {
+            return alive = true;
+        }
+    }
+
+    public int heal(int heal) {
+        if (alive = false) {
+            return this.health;
+        } else {
+            return this.health += heal;
+        }
+    }
 }
--- src/test/java/org/rpgcombat/CharacterTest.java
+++ src/test/java/org/rpgcombat/CharacterTest.java
@@ -1,6 +1,54 @@
 package org.rpgcombat;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class CharacterTest {
-  
+
+public class CharacterTest {
+  int health = 1000;
+  int level = 1;
+  boolean alive = true;
+  int damage = 100;
+
+  int heal = 300;
+  Character character = new Character(health, level, alive, damage);
+    @Test
+    public void health_should_start_with_1000() {
+
+      assertTrue(character.health >= 1000);
+    }
+    @Test
+    public void level_should_start_at_1() {
+      assertTrue(character.level == 1);
+    }
+
+    @Test
+    public void character_should_be_alive() {
+      assertTrue(character.alive);
+    }
+
+    @Test
+    public void character_should_have_damage() {
+      assertNotNull(character.damage);
+    }
+
+    @Test
+    public void damage_should_substract_from_health() {
+      assertTrue(character.attack() == 100);
+    }
+
+    @Test
+    public void should_substract_damage_from_health() {
+      assertEquals(900, character.receiveDamage(100));
+    }
+
+    @Test
+    public void if_health_is_below_0_character_should_be_dead() {
+      assertTrue(character.isAlive());
+    }
+    @Test
+    public void character_should_can_heal(){
+      assertEquals(character.heal());
+    }
+
 }
